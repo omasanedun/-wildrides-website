e1fd22c6b9cd3b1d3460297c22f7aeb2897155d1
Omasan <omasan.edun@gmail.com>
2023-03-09 23:55:09 +0000

    updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // eu-west-1_pTi8DXSUJ
-        userPoolClientId: '', // 6dve8oapgit7vtfn2pnpq3g9bi
-        region: '' // eu-west-1
+        userPoolId: 'eu-west-1_pTi8DXSUJ', // e.g. eu-west-1_pTi8DXSUJ
+        userPoolClientId: '6dve8oapgit7vtfn2pnpq3g9bi', // e.g. 6dve8oapgit7vtfn2pnpq3g9bi
+        region: 'eu-west-1' // e.g. eu-west-1
     },
     api: {
         invokeUrl: '' // https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
